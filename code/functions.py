8d9e51771fb5c3756c854c7726e157fc7a17e6a9
akomarla <124313756+akomarla@users.noreply.github.com>
2023-05-24 18:15:14 -0700

    Update all

--- code/functions.py
+++ code/functions.py
@@ -1,167 +1,168 @@
 # -*- coding: utf-8 -*-
 
-# Functions
-
 import pandas as pd
 import numpy as np
+import logging
+import os
 
 
-def read_quote_data(read_file_path):
-    # Reading Excel workbook (all tabs) at the specified path
-    odm_data = pd.read_excel(read_file_path, None)
-    return odm_data
-
-
-def set_site_program_data(site_name, program, odm_data):
+def set_site_name(df, site_name):
     # Setting the correct ODM site name for the program
-    odm_data[program].rename(columns={'Unnamed: 0': 'Site'}, inplace = True)
-    odm_data[program]['Site'] = site_name  
-    return 
-
-
-def basic_odm_data_preparation(read_file_path, site_name):
-    # Read ODM data and fix site names
-    odm_data = read_quote_data(read_file_path = read_file_path)
-    for program in odm_data.keys():
-        set_site_program_data(site_name = site_name, program = program, odm_data = odm_data)
-    return odm_data
+    df.rename(columns={'Unnamed: 0': 'ODM'}, inplace = True)
+    df['ODM'] = site_name  
+    return df
         
 
-def filter_build_status_program_data(build_status_allowed, program, odm_data):
-    # Extract data with appropriate build statuses only
-    odm_data[program].rename(columns={odm_data[program].columns[1]: 'Build Status'}, inplace = True)
-    # Only select entries whose build status is permitted
-    odm_data[program] = odm_data[program][odm_data[program]['Build Status'].isin(build_status_allowed)]
-    return
-
-
-def filter_ww_program_data(ww_range_allowed, ww_col, program, odm_data):
+def filter_ww(df, ww_range_allowed, ww_col):
     # Filter program data by ww range
-    odm_data[program] = odm_data[program][(odm_data[program][ww_col] >= ww_range_allowed[0]) & 
-                                          (odm_data[program][ww_col] <= ww_range_allowed[1])]
-    return
+    df = df[(df[ww_col] >= ww_range_allowed[0]) & (df[ww_col] <= ww_range_allowed[-1])]
+    return df
+
+
+def filter_build_status(df, build_status_allowed):
+    # Extract data with appropriate build statuses only
+    df = df.rename(columns={df.columns[1]: 'Build Status'})
+    # Only select entries whose build status is permitted
+    df = df[df['Build Status'].isin(build_status_allowed)]
+    return df
 
 
 
-def gen_all_program_names(all_odm_data, ignore_vals):
-    # Extract all program names across all ODMs
-    program_names = []
-    for odm_data in all_odm_data:
-        # Using tab names of the Excel workbook for program names
-        program_names.extend(odm_data.keys())
-    # Removing duplicate program names
-    program_names = list(set(program_names))
+def weighted_mean(values, weights):
+    # Computing weighted mean when inputs have NaN values
+    values_indices = ~np.isnan(values)
+    weights_indices = ~np.isnan(weights)
+    valid_indices = []
+    for i in range(0,len(values_indices)):
+        if values_indices[i] == True & weights_indices[i] == True:
+            valid_indices.append(True)
+        else:
+            valid_indices.append(False)
+    # Compute weighted average ignoring NaN values
+    wm = np.average(values[valid_indices], weights = weights[valid_indices])
+    return wm
+
+
+
+def compute_forecast(values, weights, how):
     
-    # Remove program names to ignore as specified by user
-    for val in ignore_vals:
-        program_names.remove(val)
-    return program_names
+    # Convert inputs to arrays
+    values = np.array(values)
+    weights = np.array(weights)
     
-
-
-def extract_program_data(all_odm_data, program, print_cond): 
-    # Find data from all ODMs for a given program 
-    program_data = pd.DataFrame()
-    for odm_data in all_odm_data:
-        if program in odm_data.keys():
-            if len(program_data) == 0:
-                # Initializing the program data dataframe
-                program_data = odm_data[program]
-            else:
-                program_data = pd.concat([program_data, odm_data[program]])
-    if print_cond == 'True':
-        print('Program ', program, 'data was found for sites: ', program_data['Site'].unique())
-    return program_data
-    
-
-
-def avg_by_prod_code(odm_data, program, subtotal_col, cost_col, print_cond):
-    # Initializing df and lists to hold family, product code, avg. subtotals data, etc.
-    prod_code_avg = pd.DataFrame(columns = ['Family', 'Site', 'Product Code', ('Avg. of: '+ subtotal_col), ('Avg. of: '+ cost_col)])
-    prod_code = []
-    family = []
-    site = []
-    avg_subtotal = []
-    avg_cost = []
-    # Calculating averages per product code for a given program name
-    program_data = odm_data[program]
-    for code in program_data['Product Code'].unique():
-        prod_code_data = program_data[program_data['Product Code'] == code]
-        prod_code.append(code)
-        # Only storing the first value of the unique values assuming that for each program there can only be one site and family name
-        family.append(prod_code_data['Family'].unique()[0])
-        site.append(prod_code_data['Site'].unique()[0])
-        # Calculating avg. subtotal by product code
-        try: 
-            avg_subtotal.append(np.mean(prod_code_data[subtotal_col]))
-            if print_cond == 'True':
-                print('The average of ', subtotal_col, ' for programs', program_data['Family'].unique(), 
-                      ' and product code', code, 'is', (np.mean(prod_code_data[subtotal_col])))
-        except:
-            avg_subtotal.append(np.nan)
-        # Calculating avg. cost by product code
+    if how == 'mean':
+        # Try except block is needed since the input values or weights could be empty or zero
         try:
-            avg_cost.append(np.mean(prod_code_data[cost_col]))
-            if print_cond == 'True':
-                print('The average of', cost_col, ' for programs', program_data['Family'].unique(), 
-                      ' and product code', code, 'is', (np.mean(prod_code_data[cost_col])))
+            # Calculating average without weights and ignoring NaN values
+            ft = np.mean(values[~np.isnan(values)])
         except:
-            avg_cost.append(np.nan)
+            ft = np.nan
+    
+    # Calculating average with weights
+    elif how == 'weighted mean':
+        # Try except block is needed since the input values or weights could be empty or zero
+        try:
+            # Compute weighted average ignoring NaN values
+            ft = weighted_mean(values = values, weights = weights)
+        except:
+            ft = np.nan
             
-    # Assigning values to output data
-    prod_code_avg['Family'] = family
-    prod_code_avg['Site'] = site
-    prod_code_avg['Product Code'] = prod_code
-    prod_code_avg[('Avg. of: '+ subtotal_col)] = avg_subtotal
-    prod_code_avg[('Avg. of: '+ cost_col)] = avg_cost
-    return prod_code_avg
-
-
-
-def write_to_excel(write_file_path, avg_cols, df):
-    df.to_excel(write_file_path)
-    print('Data on programs, product codes, averages for the following columns have been written to the specified Excel file: ')
-    for col in avg_cols:
-        print('Variable (column) name:', col)
-
-
-
-def prod_code_avg_all_programs(read_file_path, write_file_path, ignore_vals, site_name, build_status_allowed, ww_range_allowed, 
-                              subtotal_col, cost_col, ww_col, print_cond):
-    # Use this function for a single ODM at a time
-
-    # Display process start to user
-    print('Data processing started for ODM: ', site_name)
+    # Return error if forecasting method is not recognized
+    else:
+        logger.error('Forecast method requested is not supported. Contact product owner for support')
+        
+    return ft
     
-    # Do basic data preparation
-    odm_data = basic_odm_data_preparation(read_file_path = read_file_path, site_name = site_name)
-    # Display success of data preparation
-    print('Data for all programs in the ODM has been extracted.', 'Site name is assigned as: ', site_name)
-    
-    # Initialize df to hold averages for all programs (and all product codes within a program)
-    all_program_prod_code_avg = pd.DataFrame()
+        
 
-    # Loop through programs
-    for program in odm_data.keys():
-        if program not in ignore_vals:
-            # Filter on build status
-            filter_build_status_program_data(build_status_allowed = build_status_allowed, program = program, odm_data = odm_data)
-            # Filter on ww
-            filter_ww_program_data(ww_range_allowed = ww_range_allowed, ww_col = ww_col, program = program, odm_data = odm_data)
-            # Compute avg of product code in the given program
-            prod_code_avg = avg_by_prod_code(odm_data = odm_data, program = program, subtotal_col = subtotal_col, cost_col = cost_col, print_cond = print_cond)
-            # Combine product code avgs for all programs
-            all_program_prod_code_avg = pd.concat([all_program_prod_code_avg, prod_code_avg])
+def join_odm_data(write_file_path, site_name, list_odm_data, excel_output):
+    # Combine data from multiple ODMs
+    all_odm_data = pd.DataFrame()
+    for odm_data in list_odm_data:
+        all_odm_data = pd.concat([all_odm_data, odm_data])
+        
+    # Reset index
+    all_odm_data.reset_index(drop = True, inplace = True)
     
-    # Format final output
-    all_program_prod_code_avg.reset_index(drop = True, inplace = True)
-    print('Only builds with the specified WW range: ', ww_range_allowed, 'and build status: ', build_status_allowed, 'have been used for the forecast')
+    # Write output to Excel
+    if excel_output:
+       all_odm_data.to_excel(excel_writer = write_file_path, sheet_name = 'Forecasts for '+ site_name)
     
-    # Write the output to an excel file
-    write_to_excel(write_file_path = write_file_path, avg_cols = [subtotal_col, cost_col], 
-                   df = all_program_prod_code_avg)
-    
-    print('End of forecasting for site: ', site_name)
-    print('-------\n')
+    return all_odm_data
+        
 
-    return all_program_prod_code_avg
+
+def setup_logger(log_file_path):
+    # Create and configure logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+   
+    # Define file handler and set formatter
+    file_handler = logging.FileHandler(log_file_path)
+    formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
+    file_handler.setFormatter(formatter)
+    
+    # Add file handler to logger
+    logger.addHandler(file_handler)
+    return logger
+    
+
+
+def gen_odm_forecast(read_file_path, ignore_sheets, excel_output, write_file_path, log_file_path,
+                     site_name, ww_range_allowed, ww_col, build_status_allowed, 
+                     level, ft_method, weight_col):
+    
+    # Create and configure logger
+    logger = setup_logger(log_file_path = log_file_path)
+    
+    # Initialize list for forecasts
+    ft_data = []
+    
+    # Document cwd
+    logger.info('Python script located in: '+os.getcwd()+' is being executed')
+    
+    # Read ODM data
+    odm_data = pd.read_excel(read_file_path, None)
+    logger.info('Forecasting begins for '+site_name)
+    logger.info('Data for ODM has been read from: '+read_file_path)
+       
+    # Loop through programs (sheets of the odm data dictionary)
+    for program in list(set(odm_data.keys())-set(ignore_sheets)):
+        # Extract data for program family and apply filters and modifications
+        program_data = set_site_name(df = odm_data[program], site_name = site_name)
+        program_data = filter_ww(df = program_data, ww_range_allowed = ww_range_allowed, ww_col = ww_col)
+        program_data = filter_build_status(df = program_data, build_status_allowed = build_status_allowed)
+        
+        # Loop through values of the level at which to forecast (family, product code etc.)
+        for val in program_data[level].unique():
+            # Loop through quantities to forecast
+            for var in ft_method.keys():
+                # Loop through methods to use for forecast
+                for how in ft_method[var]:
+                    # Calculate the forecast for every combination of parameters
+                    ft = compute_forecast(values = program_data[program_data[level] == val][var], 
+                                          weights = program_data[program_data[level] == val][weight_col],
+                                          how = how)
+                    
+                    # Create a dictionary of forecasted values at each iteration
+                    ft_dict = {'ODM': site_name, 'Program': program, level: val, 'Forecast for: '+var+' ('+how+')': ft,
+                             'WW Start': ww_range_allowed[0], 'WW End': ww_range_allowed[-1],
+                             'Build Status Used':', '.join(build_status_allowed)}
+                    # Add the dictionary to a list
+                    ft_data.append(ft_dict)
+    
+    # Convert list of forecasts to a dataframe and coalesce so that there are no duplicate level values 
+    ft_odm = pd.DataFrame(ft_data).groupby(by = level).max().reset_index()
+    logger.info('WW filtered as greater than '+str(ww_range_allowed[0])+' and less than '+str(ww_range_allowed[-1]))
+    logger.info('Build statuses allowed are: '+', '.join(build_status_allowed))
+    logger.info('Forecasting is complete for '+site_name)
+    
+    # Write to an excel file if requested
+    if excel_output:
+        ft_odm.to_excel(excel_writer = write_file_path, sheet_name = 'Forecasts for '+site_name)
+        logger.info('Forecast output is written to: '+write_file_path+'\n -------')
+    else:
+        logger.info('Forecast output is not written to any Excel or CSV output \n -------')
+
+    return ft_odm
+
--- code/run.py
+++ code/run.py
@@ -1,31 +1,41 @@
 # -*- coding: utf-8 -*-
 
-
 from functions import *
 
 def main():
-    # For pegatron
-    prod_code_avg_all_programs(read_file_path = "C:/Users/akomarla/OneDrive - NANDPS/Desktop/Repos/npi_quote_avgs/odm_quote_files/anchored_data/Solidigm_Pegatron NPI Quote File ww51'22_rev1.xlsm", 
-                               write_file_path = "C:/Users/akomarla/OneDrive - NANDPS/Desktop/Repos/npi_quote_avgs/odm_quote_forecast/anchored_results/NPI Pega Quote Avgs.xlsx", 
-                               ignore_vals = ['Input', 'MainSheet'], 
-                               site_name = 'PEGATRON', 
-                               build_status_allowed = ['ACTIVE', 'WIP', 'DONE'], 
-                               ww_range_allowed = [202241, 202253], 
-                               subtotal_col = 'Subtotal = NRE+\nQty*(BOM+MVA)', 
-                               cost_col = 'BOM+MVA Cost', 
-                               ww_col = 'Req WW (WW enterd)', 
-                               print_cond = 'False')
+    # For Pegatron ODM
+    pegatron_forecasts = gen_odm_forecast(read_file_path = "S:/Global Supply Planning/gbl_ops_data_analytics.npi.application.quote_forecasting/odm_quote_files/anchored_data/Solidigm Pegatron NPI Quote File.xlsm",
+                                          ignore_sheets = ['Input', 'MainSheet'], 
+                                          excel_output = True, 
+                                          write_file_path = "//npcorpgobufileshares.file.core.windows.net/bi-share/Global Supply Planning/gbl_ops_data_analytics.npi.application.quote_forecasting/odm_quote_forecast/anchored_results/NPI Pegatron Forecasts.xlsx",
+                                          log_file_path = "//npcorpgobufileshares.file.core.windows.net/bi-share/Global Supply Planning/gbl_ops_data_analytics.npi.application.quote_forecasting/odm_quote_forecast/anchored_results/forecasting_log.log",
+                                          site_name = 'PEGATRON', 
+                                          ww_range_allowed = [202241, 202253], 
+                                          ww_col = 'Req WW (WW enterd)', 
+                                          build_status_allowed = ['ACTIVE', 'WIP', 'DONE'], 
+                                          level = 'Product Code', 
+                                          ft_method = {'BOM+MVA Cost': ['mean', 'weighted mean'], 'Subtotal = NRE+\nQty*(BOM+MVA)': ['mean']},
+                                          weight_col = 'Build Qty')
 
-    # For PTI
-    prod_code_avg_all_programs(read_file_path = "C:/Users/akomarla/OneDrive - NANDPS/Desktop/Repos/npi_quote_avgs/odm_quote_files/anchored_data/Solidigm_PTI TW NPI Quote File WW51'22_rev1.xlsm", 
-                               write_file_path = "C:/Users/akomarla/OneDrive - NANDPS/Desktop/Repos/npi_quote_avgs/odm_quote_forecast/anchored_results/NPI PTI Quote Avgs.xlsx",                      ignore_vals = ['Input', 'MainSheet'], 
-                               site_name = 'PTI HS', 
-                               build_status_allowed = ['ACTIVE', 'WIP', 'DONE'], 
-                               ww_range_allowed = [202241, 202253], 
-                               subtotal_col = 'Subtotal = NRE+\nQty*(BOM+MVA)', 
-                               cost_col = 'BOM+MVA Cost', 
-                               ww_col = 'Req WW (WW enterd)', 
-                               print_cond = 'False')
+    # For PTI ODM
+    pti_forecasts = gen_odm_forecast(read_file_path = "S:/Global Supply Planning/gbl_ops_data_analytics.npi.application.quote_forecasting/odm_quote_files/anchored_data/Solidigm PTI TW NPI Quote File.xlsm",
+                                     ignore_sheets = ['Input', 'MainSheet'], 
+                                     excel_output = True,
+                                     write_file_path = "//npcorpgobufileshares.file.core.windows.net/bi-share/Global Supply Planning/gbl_ops_data_analytics.npi.application.quote_forecasting/odm_quote_forecast/anchored_results/NPI PTI Forecasts.xlsx",
+                                     log_file_path = "//npcorpgobufileshares.file.core.windows.net/bi-share/Global Supply Planning/gbl_ops_data_analytics.npi.application.quote_forecasting/odm_quote_forecast/anchored_results/forecasting_log.log",
+                                     site_name = 'PTI TAIWAN', 
+                                     ww_range_allowed = [202241, 202253], 
+                                     ww_col = 'Req WW (WW enterd)', 
+                                     build_status_allowed = ['ACTIVE', 'WIP', 'DONE'], 
+                                     level = 'Product Code', 
+                                     ft_method = {'BOM+MVA Cost': ['mean', 'weighted mean'], 'Subtotal = NRE+\nQty*(BOM+MVA)': ['mean']},
+                                     weight_col = 'Build Qty')
+    
+    # Combining data for both ODMs
+    pegtraon_pti_forecasts = join_odm_data(write_file_path = "//npcorpgobufileshares.file.core.windows.net/bi-share/Global Supply Planning/gbl_ops_data_analytics.npi.application.quote_forecasting/odm_quote_forecast/anchored_results/NPI PTI & Pega Forecasts.xlsx",
+                                           site_name = 'PEGATRON & PTI TAIWAN',
+                                           list_odm_data = [pegatron_forecasts, pti_forecasts],
+                                           excel_output = True)
 
     
 if __name__ == "__main__":
